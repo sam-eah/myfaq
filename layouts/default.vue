c059dfc5d50d013b6c930e50d8ef2bf266cc596f
Samy Echarif <samy@hawaii.fr>
2020-07-30 13:55:53 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      title: 'my faq'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <!-- <v-flex
+      xs12
+      sm10
+    > -->
+    <v-text-field
+          v-model="label"
+          outlined
+          placeholder="Rechercher..."
+          clearable
+          append-icon="mdi-magnify"
+        ></v-text-field>
+      <v-expansion-panels>
+    <v-expansion-panel
+      v-for="item in myFilteredData"
+      :key="item.question"
+    >
+      <v-expansion-panel-header>{{ item.question }}</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        {{ item.answer }}
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+      
+    <!-- </v-flex> -->
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+  data: () => ({
+    label: "",
+    myData: [
+      {   
+        question: "Quel est ma couleur préférée ?",
+        answer: "Ma couleur préférée est le rouge"
+      },
+      {
+        question: "Quelle est ma chanson préférée ?",
+        answer: "Ma chanson préférée est Aline de Christophe"
+      }
+    ]
+  }),
+  computed: {
+    myFilteredData() {
+      return this.myData.filter(item => 
+        !this.label
+        || (item.question.includes(this.label)) 
+        || (item.answer.includes(this.label))
+      )
+    }
+  }
+}
+</script>
